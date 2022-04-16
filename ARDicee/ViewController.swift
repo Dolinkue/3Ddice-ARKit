1b2e193983fcf7a0a47ab71ccf64fa3a8f55cd8d
dolinkue <dolinkue_n@me.com>
2022-04-16 19:19:11 -0300

    crenado el dado 3d

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -19,14 +19,43 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+//        // creamos el cubo desde xcode
+//        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+//
+//        let material = SCNMaterial()
+//
+//        // para entrar dentro de las propiedades del cubo, el material, son propiedades dentro de ARkit
+//        material.diffuse.contents = UIColor.red
+//
+//        // creamos un array por si le damos mas materiales al objeto, en este caso uno solo que es el color
+//        cube.materials = [material]
+//
+//        // posicion del objeto
+//        let node = SCNNode()
+//
+//        // en x,y, z
+//        node.position = SCNVector3(0, 0.1, -0.5)
+//
+//        node.geometry = cube
+//
+//        //el rootnode en la base y se le pueden ir agrendo hijos por si queres poner mas de  uno
+//        sceneView.scene.rootNode.addChildNode(node)
+        
+        
+        sceneView.automaticallyUpdatesLighting = true
+        
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let diceScene = SCNScene(named: "art.scnassets/diceColladaCopy.scn")!
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        let dicenote = diceScene.rootNode.childNode(withName: "Dice", recursively: true)
+        
+        dicenote?.position = SCNVector3(0, 0, -0.1)
+        
+        
+        sceneView.scene.rootNode.addChildNode(dicenote!)
+
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -46,29 +75,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+
 }
